93fdbecfee1fd2eda3324d21cbcadd4b0bbca0cb
etienne cherri <etienne@cherri.fr>
2020-02-10 19:41:50 +0100

    push

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,31 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @ingredient = Ingredient.new(params[:ingredient])
+    @ingredient.save
+  end
+
+  def new
+    @ingredients = Ingredient.new
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end
